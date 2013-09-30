ad1c30f458b884afcc4827a9bb9e93d76e72a4b7
AN DANG <dang.an249@gmail.com>
2013-09-29 23:39:57 -0400

    log in sucessfully

---
+++ ccs/neu/edu/andang/Crawler.java
@@ -0,0 +1,190 @@
+/*
+	Programmers: An Dang + Pedro Benedicte
+	Network Fundamentals
+	Project 2 - Fakebook Web Crawler
+
+*/
+
+package ccs.neu.edu.andang ;
+
+import java.util.List ;
+import java.util.LinkedList ;
+import java.util.Set ;
+import java.util.HashSet ;
+import java.util.Queue ;
+import java.util.Map ;
+import java.util.HashMap ;
+import com.google.common.collect.Multimap ;
+import com.google.common.collect.HashMultimap ;
+
+import java.net.UnknownHostException ;
+import java.net.SocketException ;
+import java.net.MalformedURLException;
+import java.io.IOException ;
+
+import java.net.URL ;
+
+public class Crawler {
+
+	private final String HOST = "http://cs5700.ccs.neu.edu/" ;
+	private final String LOG_IN_URL = "http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/" ;
+	private HTTPClient client ;
+	private String id ;
+	private String password ;
+	private URL rootURL ;
+	private URL logInURL ;
+	private Set<URL> visitedURL ;
+	private Queue<URL> frontierURL ;
+	private List<String> secretFlags ;
+	private Map<String, String> cookies ;
+
+	public Crawler( String id, String password ){
+		this.id = id ;
+		this.password = password ;
+		this.client = new HTTPClient() ;
+		this.visitedURL = new HashSet<URL>() ;
+		this.frontierURL = new LinkedList<URL>() ;
+		this.cookies = new HashMap<String,String>() ;
+		try{
+			this.rootURL = new URL( HOST )  ;
+			this.logInURL = new URL( LOG_IN_URL ) ;
+		}
+		catch (MalformedURLException ex){
+			throw new RuntimeException( "Internal Crawler's error " + ex.toString() ) ;
+		}
+	}
+
+	// TODO
+	// log in Fakebook and get session + csrf cookies  
+	// side-effect: change this.cookies
+	//              and propably this.visitedURL, this.frontierURL, and this.secretFlags 
+	public void login(){
+		
+		try{
+
+			HTTPRequest request = new HTTPRequest( this.logInURL ) ;
+
+			// First request to know his cookies
+			Multimap<String,String> headers = HashMultimap.create() ;
+			headers.put( "From" , "dang.an249@gmail.com" ) ;
+			request.setHeaders( headers ) ;
+
+			this.client.setRequest( request ) ;
+
+			// GETTING THE LOGIN PAGE 
+			client.doGet() ; 
+
+
+			System.out.println( client.getRequest().toString() ) ;
+			System.out.println( client.getResponse().toString() ) ;
+			System.out.println("===================") ;
+
+			this.cookies = client.getResponse().getCookies() ;
+
+			request = new HTTPRequest( this.logInURL ) ;
+
+			headers.put( "Host" , this.logInURL.getHost() ) ;
+			headers.put( "Referer" , "http://cs5700.ccs.neu.edu/accounts/login/" ) ;
+			headers.put( "Connection" , "keep-alive" ) ;
+			headers.put( "Content-Type", "application/x-www-form-urlencoded" ) ;
+			
+
+			StringBuilder body = new StringBuilder(112) ;
+
+			body.append("csrfmiddlewaretoken=") ;
+			body.append( this.cookies.get( "csrftoken" )) ;
+			body.append("&username=") ;
+			body.append( this.id ) ;
+
+			body.append("&password=") ;
+			body.append( this.password ) ;
+			body.append("&next=%2Ffakebook%2F") ;
+
+			request.setRequestBody( body.toString() ) ;
+
+			headers.put( "Content-Length", Integer.toString( request.getRequestBody().length() ) ) ;
+
+			request.setHeaders( headers ) ;						
+			request.addCookies( this.cookies ) ;
+
+			client.setRequest( request ) ;
+
+			client.doPostWithRedirect() ;
+			System.out.println( client.getResponse().toString() ) ;
+			
+			/*
+			// Second request with the body specified and the cookies
+			String s1 = "csrfmiddlewaretoken=";
+			String s2 = this.cookies.get("csrftoken");
+			String s3 = "&username=001908844&password=YEN05M01&next=%2Ffakebook%2F";
+			String body = new String(s1+s2+s3);
+			request.setRequestBody(body);
+			request.addCookies(this.cookies);
+			//System.out.println(headers.toString());
+			client.doPost(cookies) ;
+			*/
+		}
+		catch( UnknownHostException ex){
+			System.out.println("Unable to connect to " + client.getRequest().getURL() + ". Unknown host" ) ;
+		} 
+		catch( SocketException ex){
+			System.out.println( "Error with underlying protocol: " + ex.toString() ) ;
+		}
+		catch( IOException ex){
+			System.out.println( ex.toString() ) ;
+		}
+
+	}
+
+	// TODO
+	// crawl the Fakebook
+	// side-effect: modify this.visitedURL, this.frontierURL, this.secretFlags
+	public void crawl(){
+
+		// check to see if cookies is set otherwise throw error
+
+		// make the GET call
+
+		// HTTP client going to handle any kind redirect for you and give back the correct
+		// response
+		// so only deal with 403 and 500 here
+
+		// parse the HTML to:
+		// -- find the keys
+		// -- enqueue new frontier URL (make sure you do not have duplicates)
+
+	}
+
+	// TODO
+	// return true if we already visit this link
+	private boolean URLVisited(URL u){
+		return false ;
+	}
+
+	// TODO
+	// print to StdOut all keys in this.secretFlags
+	public void printSecretKeys(){
+		
+	}
+
+	// TODO
+	public String getSessionCookieName(){
+		return "" ;
+	}
+
+	// TODO
+	public String getSessionCookieValue(){
+		return "" ;
+	}
+
+	public static void main(String args[]){
+
+		Crawler crawler = new Crawler( args[0], args[1]) ;
+
+		crawler.login() ;
+
+		//crawler.crawl() ;
+
+		//crawler.printSecretKeys() ;
+	}
+}
---
+++ ccs/neu/edu/andang/HTTPRequest.java
@@ -0,0 +1,97 @@
+package ccs.neu.edu.andang ;
+
+import java.net.URL ;
+import java.util.Map ;
+import com.google.common.collect.Multimap ;
+import com.google.common.collect.HashMultimap ; 
+import java.lang.StringBuilder ;
+
+public class HTTPRequest{
+
+	private URL url ; // if users do not specify port, URL default it to 80
+	private String requestBody ;
+	private Multimap< String, String > headers ;
+	private Map<String, String> cookies ;
+
+	public HTTPRequest( URL url ){
+
+		this.url = url ;
+		this.headers = HashMultimap.create() ;
+		this.requestBody = "" ;
+
+	}
+
+
+	// adding cookies to the header of sending request
+	// side-effect: add more pairs in this.headers
+	public void addCookies(Map<String, String> cookies) {
+
+		this.cookies = cookies ;
+		System.out.println("===================") ;
+		System.out.println( cookies.toString() ) ;
+		System.out.println("===================") ;
+
+		String key = "Cookie";
+		StringBuilder builder = new StringBuilder() ;
+
+		int i = 0 ;
+		
+		// TODO: cookie can be Cookie: value1; value2; name1=value1
+		// this implementation cannot deal with that yet :(
+
+		for( String cookieKey : cookies.keySet() ){
+
+				builder.append( cookieKey ) ;
+				builder.append( "=") ;
+				builder.append( cookies.get( cookieKey ) )  ;
+
+			if( i != cookies.size() - 1 ){
+				builder.append( "; ") ;
+			}
+
+			i++ ;
+		}
+
+		this.headers.put( key, builder.toString() );
+
+	}
+
+	public void setURL(URL url){
+		this.url = url ;
+	}
+
+	public URL getURL(){
+		return this.url ;
+	}
+
+	public void setRequestBody(String requestBody){
+		this.requestBody = requestBody ;
+	}
+
+	public String getRequestBody(){
+		return this.requestBody ;
+	}
+
+	public void setHeaders( Multimap< String, String > headers){
+		this.headers = headers ;
+	}
+
+	public Multimap< String, String > getHeaders(){
+		return this.headers ;
+	}
+
+	public Map<String, String> getCookies(){
+		return this.cookies ;
+	}
+	
+	@Override
+	public String toString(){
+		return  "REQUEST :" + "\n" + 
+				"URL : " + url.toString() + "\n" 				
+				+ "HEADERS: " + "\n"
+				+ headers.toString() + "\n"  
+				+ "BODY : " + "\n" 
+				+ requestBody + "\n" ;
+	}
+
+}
